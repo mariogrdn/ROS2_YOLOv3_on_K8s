0377fad0a6e9fd80095ebd6cd0e5410d68f0a3ae
Mario Giordano <34249686+mariogrdn@users.noreply.github.com>
2021-03-06 14:56:19 +0000

    Add files via upload

---
+++ pub_entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# The environment variable indicating the path to the FastRTPS configuration
+export FASTRTPS_DEFAULT_PROFILES_FILE=/fastRTPS_profile/fastRTPS_profile_ds_talker.xml
+
+# Grab the IP of the discovery server (if not provided) and fill the configuration
+if [ -z "${DISCOVERY_SERVER_IP}" ]; then
+    DISCOVERY_SERVER_IP=$(getent hosts "${DISCOVERY_SERVER_NAME}" | awk '{ print $1 ; exit }')
+fi
+sed -i "s/__DISCOVERY_SERVER_IP__/${DISCOVERY_SERVER_IP}/" ${FASTRTPS_DEFAULT_PROFILES_FILE}
+
+# Source the ROS2 configuration file
+. /opt/ros/image_pipeline/install/setup.bash
+
+# Run the image publisher, replacing the current process to get signals
+exec ros2 run image_publisher image_publisher_node "${VIDEO_PATH}"
---
+++ yolo_entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# The environment variable indicating the path to the FastRTPS configuration
+export FASTRTPS_DEFAULT_PROFILES_FILE=/fastRTPS_profile/fastRTPS_profile_ds_talker.xml
+
+# Grab the IP of the discovery server (if not provided) and fill the configuration
+if [ -z "${DISCOVERY_SERVER_IP}" ]; then
+    DISCOVERY_SERVER_IP=$(getent hosts "${DISCOVERY_SERVER_NAME}" | awk '{ print $1 ; exit }')
+fi
+sed -i "s/__DISCOVERY_SERVER_IP__/${DISCOVERY_SERVER_IP}/" ${FASTRTPS_DEFAULT_PROFILES_FILE}
+
+# Grab the IP of the yolo service (if not provided) and fill the configuration
+if [ -z "${YOLO_SERVICE_IP}" ]; then
+    YOLO_SERVICE_IP=$(getent hosts "${YOLO_SERVICE_NAME}" | awk '{ print $1 ; exit }')
+fi
+sed -i "s/__YOLO_SERVICE_IP__/${YOLO_SERVICE_IP}/" ${FASTRTPS_DEFAULT_PROFILES_FILE}
+
+# Source the ROS2 configuration file
+. /opt/ros/darknet_ros_tiny/install/setup.bash
+
+# Run the image publisher, replacing the current process to get signals
+exec ros2 launch darknet_ros darknet_ros.launch.py"
